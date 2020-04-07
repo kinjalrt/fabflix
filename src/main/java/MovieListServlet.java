3c4dbbd90d37de9639f06631bc1524fea0847425
Kinjal <kinjalreetoo@gmail.com>
2020-04-06 22:46:34 -0700

    Initial commit

---
+++ src/main/java/MovieListServlet.java
@@ -0,0 +1,62 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// this annotation maps this Java Servlet Class to a URL
+@WebServlet("/stars")
+public class MovieListServlet extends HttpServlet implements Parameters {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // change this to your own mysql username and password
+        String loginUser = "mytestuser";
+        String loginPasswd = "mypassword";
+      //  String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+
+        // set response mime type
+        response.setContentType("text/html");
+
+        // get the printwriter for writing response
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>Fabflix</title></head>");
+
+        try {
+
+            out.println("<body>i own you</body>");
+
+
+        } catch (Exception e) {
+            /*
+             * After you deploy the WAR file through tomcat manager webpage,
+             *   there's no console to see the print messages.
+             * Tomcat append all the print messages to the file: tomcat_directory/logs/catalina.out
+             *
+             * To view the last n lines (for example, 100 lines) of messages you can use:
+             *   tail -100 catalina.out
+             * This can help you debug your program after deploying it on AWS.
+             */
+            e.printStackTrace();
+
+            out.println("<body>");
+            out.println("<p>");
+            out.println("Exception in doGet: " + e.getMessage());
+            out.println("</p>");
+            out.print("</body>");
+        }
+
+        out.println("</html>");
+        out.close();
+
+    }
+
+
+}
---
+++ src/main/java/Parameters.java
@@ -0,0 +1,6 @@
+public interface Parameters {
+    String dbtype = "mysql";
+    String dbname = "moviedb";
+    String username = "mytestuser";
+    String password = "mypassword";
+}
