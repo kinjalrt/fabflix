7651aa717923b68c61abb287e1cb6a6034e7ccb3
Kinjal <kinjalreetoo@gmail.com>
2020-04-19 10:23:02 -0700

    p2 search feat complete

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -1,5 +1,6 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.sun.deploy.security.SelectableSecurityManager;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -35,88 +36,131 @@ public class MovieListServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         try {
+            //get parameters from url
+            String param_title = request.getParameter("title");
+            String param_year = request.getParameter("year");
+            String param_dir = request.getParameter("director");
+            String param_star = request.getParameter("star");
+
             // Get a connection from dataSource
             Connection dbcon = dataSource.getConnection();
 
-            // Declare our statement
-            Statement statement = dbcon.createStatement();
-
-            String query = "SELECT DISTINCT id, title, year, director, rating\n" +
-                    "FROM movies as m, ratings as r\n" +
-                    "WHERE m.id = r.movieId\n" +
-                    "ORDER BY rating DESC\n" +
-                    "LIMIT 20";;
-
-            // Perform the query
-            ResultSet rs = statement.executeQuery(query);
 
             JsonArray jsonArray = new JsonArray();
 
-            // Iterate through each row of rs
-            while (rs.next()) {
-                String movie_id = rs.getString("id");
-                String title = rs.getString("title");
-                int year = rs.getInt("year");
-                String dir = rs.getString("director");
-                float rating = rs.getFloat("rating");
-
-
-                // Create a JsonObject based on the data we retrieve from rs
+            //check if query empty
+            if( (param_dir.equals("")) && (param_star.equals("")) && (param_title.equals("")) && (param_year.equals(""))){
                 JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("movie_id", movie_id);
-                jsonObject.addProperty("title", title);
-                jsonObject.addProperty("year", year);
-                jsonObject.addProperty("dir", dir);
-                jsonObject.addProperty("rating", rating);
-
-
-                Statement s2 = dbcon.createStatement();
-                String q2 ="SELECT title, name\n" +
-                        "FROM movies as m, genres as g, genres_in_movies as gim\n" +
-                        "WHERE  m.title = \""+ title +"\" AND m.id = gim.movieId AND gim.genreId = g.id\n" +
-                        "LIMIT 3";
-                ResultSet r2 = s2.executeQuery(q2);
-                String genre = "genre";
-                int incr = 1;
-                while(r2.next()) {
-                    String g =  r2.getString("name");
-                    jsonObject.addProperty(genre+incr, g);
-                    incr+=1;
-                }
-                r2.close();
-                s2.close();
-
-                Statement s3 = dbcon.createStatement();
-                String q3 ="SELECT title, s.id as sid, name\n" +
-                        "FROM movies as m, stars_in_movies as sim, stars as s\n" +
-                        "WHERE  m.title = \""+ title+"\" AND m.id = sim.movieId AND sim.starId = s.id\n" +
-                        "LIMIT 3";
-                ResultSet r3 = s3.executeQuery(q3);
-                String starname = "starname";
-                String starid = "starid";
-                incr = 1;
-                while(r3.next()) {
-                    String sname =  r3.getString("name");
-                    String sid =  r3.getString("sid");
-                    jsonObject.addProperty(starname+incr, sname);
-                    jsonObject.addProperty(starid+incr, sid);
-                    incr+=1;
-                }
-                r3.close();
-                s3.close();
-
-
+                jsonObject.addProperty("result", "empty query");
                 jsonArray.add(jsonObject);
+                out.write(jsonArray.toString());
+                // set response status to 200 (OK)
+                response.setStatus(200);
             }
 
-            // write JSON string to output
-            out.write(jsonArray.toString());
-            // set response status to 200 (OK)
-            response.setStatus(200);
+            else {
 
-            rs.close();
-            statement.close();
-            dbcon.close();
+                Statement statement = dbcon.createStatement();
+                String query  = "";
+
+                if (param_year.equals("")) {
+                    query += "SELECT DISTINCT m.id, title, year, director, rating\n" +
+                            "FROM movies as m, ratings as r, stars_in_movies as sim, stars as s\n" +
+                            "WHERE m.title LIKE \"%" + param_title + "%\" AND m.id = r.movieId AND m.director LIKE \"%" + param_dir + "%\" " +
+                            "AND sim.movieId = m.id AND sim.starId = s.id AND s.name LIKE \"%" + param_star + "%\" \n" +
+                            "ORDER BY rating DESC\n" +
+                            "LIMIT 20 \n";
+                } else {
+                    query += "SELECT DISTINCT m.id, title, year, director, rating\n" +
+                            "FROM movies as m, ratings as r, stars_in_movies as sim, stars as s\n" +
+                            "WHERE m.title LIKE \"%" + param_title + "%\" AND m.id = r.movieId AND m.year = \"" + param_year + "\" AND m.director LIKE \"%" + param_dir + "%\" " +
+                            "AND sim.movieId = m.id AND sim.starId = s.id AND s.name LIKE \"%" + param_star + "%\" " +
+                            "ORDER BY  rating DESC\n" +
+                            "LIMIT 20 \n";
+                    ;
+                }
+
+
+                // Perform the query
+                ResultSet rs = statement.executeQuery(query);
+
+                //check if results > 0
+                if (!rs.isBeforeFirst()) {
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("result", "no results found for this query");
+                    jsonArray.add(jsonObject);
+
+                }
+                else {
+                    // Iterate through each row of rs
+                    while (rs.next()) {
+
+                        String movie_id = rs.getString("id");
+                        String title = rs.getString("title");
+                        int year = rs.getInt("year");
+                        String dir = rs.getString("director");
+                        float rating = rs.getFloat("rating");
+
+                        JsonObject jsonObject = new JsonObject();
+
+                        jsonObject.addProperty("result", "success");
+                        jsonObject.addProperty("movie_id", movie_id);
+                        jsonObject.addProperty("title", title);
+                        jsonObject.addProperty("year", year);
+                        jsonObject.addProperty("dir", dir);
+                        jsonObject.addProperty("rating", rating);
+
+                        //output at most genres
+                        Statement s2 = dbcon.createStatement();
+                        String q2 = "SELECT title, name\n" +
+                                "FROM movies as m, genres as g, genres_in_movies as gim\n" +
+                                "WHERE  m.title = \"" + title + "\" AND m.id = gim.movieId AND gim.genreId = g.id\n" +
+                                "LIMIT 3";
+                        ResultSet r2 = s2.executeQuery(q2);
+                        String genre = "genre";
+                        int incr = 1;
+                        while (r2.next()) {
+                            String g = r2.getString("name");
+                            jsonObject.addProperty(genre + incr, g);
+                            incr += 1;
+                        }
+                        r2.close();
+                        s2.close();
+
+                        //output all stars for each film
+                        Statement s3 = dbcon.createStatement();
+                        String q3 = "SELECT title, s.id as sid, name\n" +
+                                "FROM movies as m, stars_in_movies as sim, stars as s\n" +
+                                "WHERE  m.title = \"" + title + "\" AND m.id = sim.movieId AND sim.starId = s.id";
+                        ResultSet r3 = s3.executeQuery(q3);
+                        String starname = "starname";
+                        String starid = "starid";
+                        int count = 1;
+                        //if (!r3.isBeforeFirst() == false )
+                        while (r3.next()) {
+                            String sname = r3.getString("name");
+                            String sid = r3.getString("sid");
+                            jsonObject.addProperty(starname + count, sname);
+                            jsonObject.addProperty(starid + count, sid);
+                            count += 1;
+                        }
+                        r3.close();
+                        s3.close();
+
+                        //add each movie array
+                        jsonArray.add(jsonObject);
+                    }
+                }
+
+                    // write JSON string to output
+                    out.write(jsonArray.toString());
+                    // set response status to 200 (OK)
+                    response.setStatus(200);
+
+                rs.close();
+                statement.close();
+                dbcon.close();
+            }
         } catch (Exception e) {
 
             // write error message JSON object to output
---
+++ web/search-list.js
@@ -0,0 +1,106 @@
+let search_form = $("#search_form");
+
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs two steps:
+ *      1. Use jQuery to talk to backend API to get the json data.
+ *      2. Populate the data to correct html elements.
+ */
+
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+function handleStarResult(resultData) {
+    console.log("handleStarResult: populating star table from resultData");
+
+// Populate the star table
+// Find the empty table body by id "star_table_body"
+    let starTableBodyElement = jQuery("#movie_table_body");
+    if(resultData[0]["result"] != "success") {
+        starTableBodyElement.append(resultData[0]["result"]);
+    }
+    else {
+        // Iterate through resultData, no more than 10 entries
+        for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+            // Concatenate the html tags with resultData jsonObject
+            let rowHTML = "";
+            rowHTML += "<tr>";
+            // rowHTML += "<th>" + resultData[i]["title"] + "</th>";
+            rowHTML +=
+                '<td>' +
+                '<a href="single-movie.html?id=' + resultData[i]["movie_id"] + '">'
+                + resultData[i]["title"] +     // display star_name for the link text
+                '</a>' + '</td>';
+            rowHTML += "<td>" + resultData[i]["year"] + "</td>";
+            rowHTML += "<td>" + resultData[i]["dir"] + "</td>";
+            rowHTML += "<td>" + resultData[i]["rating"] + "</td>";
+            rowHTML += "<td>";
+            let count = 1;
+            while (resultData[i]["genre" + count] != undefined) {
+                if (count == 1)
+                    rowHTML += resultData[i]["genre" + count];
+                else
+                    rowHTML += "<br /> " + resultData[i]["genre" + count];
+                count += 1;
+            }
+            rowHTML += "</td>";
+            rowHTML += '<td>';
+            let index = 1;
+            while (resultData[i]["starname" + index] != undefined) {
+                if (index == 1)
+                    rowHTML += '<a href="single-star.html?id=' + resultData[i]["starid" + index] + '">' + resultData[i]["starname" + index] + '</a>';
+                else
+                    rowHTML += '<br /> ' + '<a href="single-star.html?id=' + resultData[i]["starid" + index] + '">' + resultData[i]["starname" + index] + '</a>';
+                index += 1;
+            }
+            rowHTML += "</td>";
+            rowHTML += "</tr>";
+
+            // Append the row created to the table body, which will refresh the page
+            starTableBodyElement.append(rowHTML);
+        }
+    }
+
+
+
+}
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+let title = getParameterByName('title');
+let year = getParameterByName('year');
+let director = getParameterByName('director');
+let star = getParameterByName('star');
+
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+$.ajax({
+    dataType: "json",
+    url: "api/top20?title="+title+"&year="+year+"&director="+director+"&star="+star,
+    method: "GET",
+    success: (resultData) => handleStarResult(resultData)
+});
+
