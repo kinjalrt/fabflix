3a65c182b2ef71b55ecb14b8230b697709aad9f0
Yasvi Patel <patelya@uci.edu>
2020-05-20 22:42:59 -0700

    Clean up

--- src/SAXParsers.java
+++
@@ -1,9 +0,0 @@
-public class SAXParsers {
-
-    public static void main(String args[]) throws Exception {
-        SAXParserMovies.main(args);
-        SAXParserStars.main(args);
-        SAXParserCast.main(args);
-    }
-
-}
